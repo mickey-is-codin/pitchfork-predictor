bec06b4f8107e109a6df34c6fea95febcd43548f
Mickey Smith <smithmick14@gmail.com>
2019-10-28 13:12:32 -0500

    Created gitignore and updated error handling. Mostly included guards around soup returning None results

--- dataset-creator/src/crawler.py
+++ dataset-creator/src/crawler.py
@@ -1,6 +1,7 @@
 import csv
 import requests
 from bs4 import BeautifulSoup
+from tqdm import tqdm
 
 csv_fname = "data/p4k_reviews.csv"
 
@@ -8,7 +9,7 @@ def main():
 
     print("\nCrawling Pitchfork for review scores\n\n")
 
-    pages_2_scrape = 10
+    pages_2_scrape = 50
 
     make_csv_header()
     scrape_review_pages(pages_2_scrape)
@@ -33,9 +34,8 @@ def scrape_review_pages(pages_2_scrape):
         page_html = requests.get(page_url).content
         page_soup = BeautifulSoup(page_html, "html.parser")
 
-        # Looking for links with class "review__link"
         page_review_divs = page_soup.findAll("div", {"class": "review"})
-        for review_div in page_review_divs:
+        for review_div in tqdm(page_review_divs):
             review_link = review_div.find("a", {"class": "review__link"})["href"]
 
             review_html = requests.get(p4k_base + review_link).content
@@ -46,11 +46,8 @@ def scrape_review_pages(pages_2_scrape):
             artist = tombstone.find("li").find("a")
             album  = tombstone.find("h1", {"class": "single-album-tombstone__review-title"})
             score  = review_soup.find("span", {"class": "score"})
-
             publish_date = review_soup.find("time", {"class": "pub-date"})["datetime"]
-
             genre = review_soup.find("a", {"class": "genre-list__link"})
-
             review_p = review_soup.find("div", {"class": "contents dropcap"}).findAll("p")
 
             if  (
@@ -70,6 +67,11 @@ def scrape_review_pages(pages_2_scrape):
                 for paragraph in review_p:
                     review_text += paragraph.text
 
+            album_lyrics = get_album_lyrics(artist, album)
+
+            if album_lyrics == None:
+                next
+
             # print(
             #     f"Artist: {artist}, \n"
             #     f"Album: {album}, \n"
@@ -78,9 +80,43 @@ def scrape_review_pages(pages_2_scrape):
             #     f"Date: {publish_date}"
             # )
 
-            csv_info = [artist, album, genre, review_text, publish_date, score]
+            csv_info = [artist, album, genre, review_text, album_lyrics, publish_date, score]
 
             append_csv(csv_info)
+
+def get_album_lyrics(artist, album):
+
+    album_lyrics = ""
+
+    url_artist = artist.replace(" ", "-").capitalize()
+    url_album  = album.replace(" ", "-").capitalize()
+
+    genius_album_url  = f"https://genius.com/albums/{url_artist}/{url_album}"
+    genius_album_html = requests.get(genius_album_url).content
+
+    album_page_soup = BeautifulSoup(genius_album_html, "html.parser")
+
+    track_rows = album_page_soup.findAll("div", {"class": "chart_row-content"})
+    for row in track_rows:
+        song_link = row.find("a", {"class": "u-display_block"})["href"]
+        song_html = requests.get(song_link).content
+        song_soup = BeautifulSoup(song_html, "html.parser")
+
+        lyrics_div = song_soup.find("div", {"class": "lyrics"})
+
+        if (lyrics_div == None):
+            return None
+        else:
+            lyrics_p = lyrics_div.find("p")
+
+            if lyrics_p == None:
+                return None
+            else:
+                lyrics_p = lyrics_p.text
+
+        album_lyrics += lyrics_p
+
+    return album_lyrics
 
 def append_csv(csv_info):
 
