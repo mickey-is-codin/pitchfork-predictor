d31258ee499b4c961a977da26631c8de71d756ac
Mickey Smith <smithmick14@gmail.com>
2019-10-28 16:24:29 -0500

    Lyrics section error control

--- dataset-creator/src/crawler.py
+++ dataset-creator/src/crawler.py
@@ -82,7 +82,10 @@ def scrape_review_pages(pages_2_scrape):
                 for paragraph in review_p:
                     review_text += paragraph.text
 
-            album_lyrics = get_album_lyrics(artist, album)
+            if (artist == "N/A" or album == "N/A"):
+                album_lyrics = "N/A"
+            else:
+                album_lyrics = get_album_lyrics(artist, album)
 
             if album_lyrics == None:
                 next
